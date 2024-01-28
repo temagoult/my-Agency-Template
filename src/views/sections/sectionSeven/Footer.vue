88ce16dc74d964d15bc56d99937dd0fabe757243
MOHQMED <mohamed.temagoult@gmail.com>
2024-01-28 23:16:19 +0100

    newComiit2

--- src/views/sections/sectionSeven/Footer.vue
+++ src/views/sections/sectionSeven/Footer.vue
@@ -1,6 +1,5 @@
 
 <template>
-<<<<<<< HEAD
   <div class="footer">
     <div class="h-[100%] grid sm:grid-cols-2 grid-cols-1 p-12 md:gap-7 sm:gap-5 gap-3 myContainer">
       <div
@@ -13,26 +12,6 @@
           <br class="md:block hidden" />of type and scrambled it to make a type specimen book. It
           <br class="md:block hidden" />has survived not only five centuries.
         </p>
-=======
- <div class="footer">
-    <div class="h-[100%] grid sm:grid-cols-2 grid-cols-1 p-12 md:gap-7 sm:gap-5 gap-3 myContainer">
-    <div
-      class="partOneFooter flex flex-col justify-evenly gap-3 sm:text-start text-center sm:items-start items-center"
-    >
-      <div class="logoFooter">Dream Agency</div>
-      <p class="text-[#FFF] lg:text-[16px] md:text-[15px] sm:text-[13px] text-[12px]">
-        Lorem Ipsum has been the industry's standard dummy text
-        <br class="md:block hidden" />ever since the 1500s, when an unknown printer took a galley
-        <br class="md:block hidden" />of type and scrambled it to make a type specimen book. It
-        <br class="md:block hidden" />has survived not only five centuries.
-      </p>
-      <div class="getInTouch lg:text-[16px] md:text-[15px] sm:text-[13px] text-[12px]">Get In Touch</div>
-      <div class="inputField flex">
-        <input
-          placeholder="Enter Your Email "
-          class="input flex p-1 lg:w-[263px] lg:h-[52px] md:w-[230px] md:h-[45px] sm:w-[200px] sm:h-[40px] w-[150px] h-[30px] placeholder:lg:text-[16px] placeholder:md:text-[15px] placeholder:sm:text-[13px] placeholder:text-[12px]"
-        />
->>>>>>> 4498aafd120db18c9cb046e780c5ad1d93b0f25b
         <div
           class="getInTouch lg:text-[16px] md:text-[15px] sm:text-[13px] text-[12px]"
         >Get In Touch</div>
